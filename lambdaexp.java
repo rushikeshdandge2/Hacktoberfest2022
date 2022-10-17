3b1a1d2aa38e6c2a9391ab49cfcb1333762b6a2f
rushikeshdandge2 <115983449+rushikeshdandge2@users.noreply.github.com>
2022-10-17 14:42:30 +0530

    Create lambdaexp.java

---
+++ lambdaexp.java
@@ -0,0 +1,12 @@
+public class FunctionInterfaceTest {
+    public static void main(String[] args) {
+
+        // anonymous class
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("I just implemented the Runnable Functional Interface.");
+            }
+        }).start();
+    }
+}
